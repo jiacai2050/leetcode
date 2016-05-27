b2ef3e6b7e5d6bc4d5773f6f506d6f69c155da86
jiacai2050 <jiacai2050@gmail.com>
2016-05-27 23:50:07 +0800

    pass 279 but in high time complexity

--- 279.py
+++ 279.py
@@ -1,8 +1,30 @@
-import math, sys
+# coding: utf-8
+# https://leetcode.com/problems/perfect-squares/
+
+# 思路：
+# 采用动态规划的思想，将原问题分拆为两个子问题
+# iter 函数中的前两个判断当前子问题是否有可能成为最优解
+
+# TODO: 降低时间复杂度，改成循环方式
+
+import math
 
 class Solution(object):
+
     def closePerfectSquares(self, n):
         return int(math.sqrt(n))
+
+    def iter(self, current, max_close_perfact, count):
+        if count >= self.best_count or max_close_perfact<1 or current < 0:
+            return self.target
+        if current - max_close_perfact*max_close_perfact*(self.best_count-count) > 0:
+            return self.target
+        if current == 0:
+            self.best_count = count
+            return count
+        else:
+            return min(self.iter(current-max_close_perfact*max_close_perfact, max_close_perfact, count+1),
+                       self.iter(current, max_close_perfact-1, count))
 
     def numSquares(self, n):
         """
@@ -11,26 +33,10 @@ class Solution(object):
         """
         if n == 0:
             return 1
-        res = sys.maxint
-
-        count = 0
-        for i in xrange(self.closePerfectSquares(n), 0, -1):
-            remaining = n
-            for j in xrange(i, 0, -1):
-                while remaining > 0:
-                    remaining -= j*j
-                    count += 1
-
-                if remaining == 0:
-                    if count < res:
-                        res = count
-                    count = 0
-                    break
-                else:
-                    remaining += j*j
-                    count -= 1
-
-        return res
+        else:
+            self.target = n
+            self.best_count = n
+            return self.iter(n, self.closePerfectSquares(n), 0)
 
 
 if __name__ == '__main__':
@@ -38,4 +44,9 @@ if __name__ == '__main__':
     assert s.numSquares(13) == 2
     assert s.numSquares(12) == 3
     assert s.numSquares(100) == 1
-    assert s.numSquares(43) == 3  # False now
+    assert s.numSquares(43) == 3
+    assert s.numSquares(1) == 1
+    assert s.numSquares(137) == 2
+    assert s.numSquares(7168) == 4
+    assert s.numSquares(19) == 3
+    assert s.numSquares(9975) == 4
