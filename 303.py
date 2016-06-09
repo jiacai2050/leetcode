d6faf0bdf29f131237323bfa6b0c681cabc9441c
jiacai2050 <jiacai2050@gmail.com>
2016-06-09 21:20:44 +0800

    pass 303

--- 303.py
+++ 303.py
@@ -1,23 +1,17 @@
 # https://leetcode.com/problems/range-sum-query-immutable/
 
-# FIX: Time Limit Exceeded
-
 class NumArray(object):
     def __init__(self, nums):
         """
         initialize your data structure here.
         :type nums: List[int]
         """
-        self.sums = []
-        len_nums = len(nums)
-        for i, n in enumerate(nums):
-            row = [0] * i
-            row.append(n)
-            j = i + 1
-            while j < len_nums:
-                row.append(row[j-1] + nums[j])
-                j += 1
-            self.sums.append(row)
+        if len(nums) == 0:
+            self.sums = [0]
+        else:
+            self.sums = [nums[0]]
+            for n in nums[1:]:
+                self.sums.append(self.sums[-1] + n)
 
     def sumRange(self, i, j):
         """
@@ -26,16 +20,13 @@ class NumArray(object):
         :type j: int
         :rtype: int
         """
-        return self.sums[i][j]
-
+        if i == 0:
+            return self.sums[j]
+        else:
+            return self.sums[j] - self.sums[i-1]
 
 if __name__ == '__main__':
     s = NumArray([-2, 0, 3, -5, 2, -1])
     assert s.sumRange(0, 2) == 1
     assert s.sumRange(2, 5) == -1
     assert s.sumRange(0, 5) == -3
-
-# Your NumArray object will be instantiated and called as such:
-# numArray = NumArray(nums)
-# numArray.sumRange(0, 1)
-# numArray.sumRange(1, 2)
